3da36044f90a41f4962000fe679316d41fbdd8f5
qreilly00 <qreilly00@gmail.com>
2024-03-19 18:51:08 +0000

    Add basic logic and callbacks for create and delete buttons.

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,37 @@ fn main() {
 
     let main_window = ChecklistApp::new().unwrap();
 
-    //println!("{}", main_window.container().len_root_Width);
+    //let mut vec_checklist: Vec<ChecklistInfo> = main_window.get_arr_checklist().iter().collect();
 
-    let mut vec_checklist: Vec<ChecklistInfo> = main_window.get_arr_checklist().iter().collect();
-    vec_checklist.push(ChecklistInfo::clone(&ChecklistInfo {
-        str_title: "Checklist 1".into(),
-    }));
+    let main_window_weak = main_window.as_weak().unwrap();
+    main_window.on_add_checklist(move || {
+        let mut vec_checklist: Vec<ChecklistInfo> = main_window_weak.get_arr_checklist().iter().collect();
+        vec_checklist.push(ChecklistInfo::clone(&ChecklistInfo {str_title: "Checklist".into(),}));
 
-    vec_checklist.push(ChecklistInfo::clone(&ChecklistInfo {
-        str_title: "Checklist 2".into(),
-    }));
+        let checklist_model = std::rc::Rc::new(slint::VecModel::from(vec_checklist));
+        main_window_weak.set_arr_checklist(checklist_model.into());
+    });
 
-    let checklist_model = std::rc::Rc::new(slint::VecModel::from(vec_checklist));
-    main_window.set_arr_checklist(checklist_model.into());
+    let main_window_weak = main_window.as_weak().unwrap();
+    main_window.on_remove_all_checklist(move || {
+        let mut vec_checklist: Vec<ChecklistInfo> = Vec::new();
+
+        let checklist_model = std::rc::Rc::new(slint::VecModel::from(vec_checklist));
+        main_window_weak.set_arr_checklist(checklist_model.into());
+    });
+
+    let main_window_weak = main_window.as_weak().unwrap();
+    main_window.on_add_checkable_item(move || {
+        let mut vec_checkable_item: Vec<CheckableItemInfo> = main_window_weak.get_arr_checkable_item().iter().collect();
+
+        vec_checkable_item.push(CheckableItemInfo::clone(&CheckableItemInfo {}));
+
+        let checklist_model = std::rc::Rc::new(slint::VecModel::from(vec_checkable_item));
+        main_window_weak.set_arr_checkable_item(checklist_model.into());
+    });
+
+    /*main_window.invoke_add_checklist();
+    main_window.invoke_add_checklist();*/
 
     main_window.run().unwrap();
 }
